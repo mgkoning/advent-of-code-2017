2cb92dad3d98cebd8cf5c34fb22336836e72782f
Michiel Koning <m.g.koning@gmail.com>
2017-11-30 21:35:48 +0100

    made 'howLong' more readable

--- playground/test.hs
+++ playground/test.hs
@@ -8,15 +8,25 @@ take5 x = take 5 x [] where
   take 0 _ ys = ys
   take n (x:xs) ys = take (n-1) xs (x:ys)
 
+data TimeParts = TimeParts { hours :: Integer, minutes :: Integer, seconds :: Integer }
+instance Show TimeParts where
+  show a = (show $ hours a) ++ " hours, " ++ (show $ minutes a) ++ " minutes, " ++ (show $ seconds a) ++ " seconds"
+
 howLong = do
-  x <- left
-  let secondsLeft = floor x
-  let (hours, minutes, seconds) = (rem (quot secondsLeft (60*60)) 60, rem (quot secondsLeft 60) 60, rem secondsLeft 60)
-  putStrLn ((show hours) ++ " hours, " ++ (show minutes) ++ " minutes, " ++ (show seconds) ++ " seconds left.")
+  now <- getCurrentTime
+  let secondsLeft = floor $ timeLeft now
+  if (secondsLeft < 0) then do
+    putStrLn "It has started!"
+  else do
+    putStrLn ((show $ timeParts secondsLeft) ++ " left.")
   where
-    now = getCurrentTime
-    startTime = zonedTimeToUTC (ZonedTime (LocalTime (dec1) (midnight)) est)
+    timeLeft = diffUTCTime startTime
+    startTime = zonedTimeToUTC $ ZonedTime (LocalTime dec1 midnight) est
     dec1 = fromGregorian 2017 12 1
     est = TimeZone (-5*60) False "EST"
-    left = fmap (diffUTCTime startTime) now
+    timeParts timeDiff = TimeParts
+      {hours = rem (quot timeDiff (60*60)) 60,
+       minutes = rem (quot timeDiff 60) 60,
+       seconds = rem timeDiff 60}
+
 
