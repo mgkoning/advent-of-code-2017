0bb91a25f1d9a216c466486db92ff1df70936635
Michiel Koning <m.g.koning@gmail.com>
2017-12-07 08:24:13 +0100

    day 7: recursive circus (part 2: added disc data type)

--- day-7-recursive-circus/recursive-circus.hs
+++ day-7-recursive-circus/recursive-circus.hs
@@ -1,4 +1,7 @@
 import Data.List
+import qualified Data.Map.Strict as Map
+
+data Disc = Disc {name :: String, weight :: Int, supported :: [Disc]}
 
 readInput :: String -> [(String, Int, [String])]
 readInput input = [(name, weight, if null supported then supported else tail supported) |
@@ -9,10 +12,16 @@ readInput input = [(name, weight, if null supported then supported else tail sup
   where
     wordsByLine = map words $ lines input
 
+buildTree :: [(String, Int, [String])] -> Disc
+buildTree s = Disc "not done" 0 []
+  where
+    programsMap = Map.fromList (map (\(n,w,s) -> (n, (n, w, s))) s)
+    discMap = Map.empty :: Map.Map String [Disc]
+  
+
 solution = do
   inputString <- readFile "input.txt"
   let input = readInput inputString
-  --putStrLn $ show $ input
   let allPrograms = map (\(x, _, _) -> x) input
   let supportedPrograms = foldl1 (++) $ map (\(_, _, z) -> z) input
   putStrLn "Part 1:"
