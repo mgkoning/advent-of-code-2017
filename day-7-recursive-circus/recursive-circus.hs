a08a30394bafb2695f30449b2df49a77b038a7c9
Michiel Koning <m.g.koning@gmail.com>
2017-12-07 08:25:56 +0100

    day 7: one more dropWhile

--- day-7-recursive-circus/recursive-circus.hs
+++ day-7-recursive-circus/recursive-circus.hs
@@ -4,11 +4,11 @@ import qualified Data.Map.Strict as Map
 data Disc = Disc {name :: String, weight :: Int, supported :: [Disc]}
 
 readInput :: String -> [(String, Int, [String])]
-readInput input = [(name, weight, if null supported then supported else tail supported) |
+readInput input = [(name, weight, supported) |
     line <- wordsByLine,
     let name = head line,
     let weight = read $ takeWhile (/=')') $ dropWhile (=='(') $ head $ tail line,
-    let supported = map (takeWhile (/=',')) $ dropWhile (/="->") line]
+    let supported = map (takeWhile (/=',')) $ dropWhile (=="->") $ dropWhile (/="->") line]
   where
     wordsByLine = map words $ lines input
 
