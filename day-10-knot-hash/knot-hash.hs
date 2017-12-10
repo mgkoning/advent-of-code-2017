d14ba6d4bc7c893a73b38ef48fac6101d186753a
Michiel Koning <m.g.koning@gmail.com>
2017-12-10 12:12:06 +0100

    day 10: knot hash, part 2

--- day-10-knot-hash/knot-hash.hs
+++ day-10-knot-hash/knot-hash.hs
@@ -1,8 +1,16 @@
+import Data.Bits
+import Data.Char
 import Data.List
 import Data.List.Split
+import Numeric
 
-input :: [Int]
-input = map read $ splitOn "," "230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167"
+puzzleInput = "230,1,2,221,97,252,168,169,57,99,0,254,181,255,235,167"
+
+inputAsInts :: [Int]
+inputAsInts = map read $ splitOn "," puzzleInput
+
+inputAsAscii :: [Int]
+inputAsAscii = map ord $ puzzleInput
 
 transformations :: [Int] -> [(Int, Int)]
 transformations input = zip input [0..]
@@ -17,13 +25,29 @@ tieKnot state (stepLength, skipSize) =
 rotateLeft :: Int -> [Int] -> [Int]
 rotateLeft n xs = take (length xs) $ drop n (cycle xs)
 
-knotHash :: [Int] -> [Int] -> [Int]
-knotHash input startState = result
+knotHash :: [Int] -> [Int]
+knotHash input = result
   where
+    startState = [0..255]
     transformResult = foldl' tieKnot startState (transformations input)
     effectiveShift = (sum $ zipWith (+) input [0..]) `rem` length startState
     result = rotateLeft (length startState - effectiveShift) transformResult
 
+knotHashPart2 :: [Int] -> [Int]
+knotHashPart2 input =
+  let hashInput = input ++ [17, 31, 73, 47, 23]
+      hash = knotHash (take (64 * length hashInput) (cycle hashInput))
+      denseHash = map (foldl1' xor) (chunksOf 16 hash)
+  in denseHash
+
+showHash :: [Int] -> String
+showHash = concat . (map (reverse . (take 2) . reverse . ("00"++) . (`showHex` "")))
+
 solve = do
-  let hash = knotHash input [0..255]
+  let hash = knotHash inputAsInts
+  putStrLn "Part 1:"
   putStrLn $ show $ product $ take 2 hash
+
+  putStrLn "\nPart 2:"
+  let hash2 = knotHashPart2 inputAsAscii
+  putStrLn $ showHash hash2
