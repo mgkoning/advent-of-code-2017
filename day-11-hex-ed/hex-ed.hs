e4a5e12598dfaa6387eededededd0a50a1a08b49
Michiel Koning <m.g.koning@gmail.com>
2017-12-11 08:00:19 +0100

    day 11: hex ed

---
+++ day-11-hex-ed/hex-ed.hs
@@ -0,0 +1,39 @@
+import Data.List
+import Data.List.Split
+
+{-
+  This solution uses three-axis cube coordinates as described here:
+  https://www.redblobgames.com/grids/hexagons/
+  One of the values in the coordinates is actually redundant, because in
+  that system every hex satisfies x + y + z = 0. However, for ease of implementation
+  and understanding, I just used all three.
+-}
+
+addTripel :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+addTripel (a, b, c) (d, e, f) = (a+d, b+e, c+f)
+
+hammingDistance :: (Int, Int, Int) -> Int
+hammingDistance (x, y, z) = (abs x + abs y + abs z) `quot` 2
+
+hexDirection :: String -> (Int, Int, Int)
+hexDirection s =
+  case s of
+    "n"  -> ( 0,  1, -1)
+    "ne" -> ( 1,  0, -1)
+    "se" -> ( 1, -1,  0)
+    "s"  -> ( 0, -1,  1)
+    "sw" -> (-1,  0,  1)
+    "nw" -> (-1,  1,  0)
+
+distance :: String -> Int
+distance = hammingDistance . foldl1' addTripel . map hexDirection . splitOn ","
+
+furthestEver :: String -> Int
+furthestEver = maximum . map hammingDistance . scanl1 addTripel . map hexDirection . splitOn ","
+
+solve = do
+  input <- readFile "input.txt"
+  putStrLn "Part 1:"
+  putStrLn $ show $ distance input
+  putStrLn "\nPart 2:"
+  putStrLn $ show $ furthestEver input
