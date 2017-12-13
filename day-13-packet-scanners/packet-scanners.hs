fefd95f3f9b95c25c646d77c9f0631bbd180db10
Michiel Koning <m.g.koning@gmail.com>
2017-12-13 08:36:22 +0100

    day 13: packet scanners (part 2 does not work - need to find bug)

---
+++ day-13-packet-scanners/packet-scanners.hs
@@ -0,0 +1,29 @@
+import Data.Maybe
+
+readInput :: String -> [(Int, Int)]
+readInput = (map parseLine) . lines
+  where parseLine s = (read $ takeWhile (/=':') s, read $ tail $ dropWhile (/=':') s)
+
+severity :: [(Int, Int)] -> Int -> Int
+severity gates delay = sum $ map score [0..maxGate]
+  where
+    maxGate = fst (last gates)
+    score t =
+      case lookup t gates of
+        Nothing -> 0
+        Just range -> if caught then t * range else 0
+          where caught = 0 == positionAt (t+delay) range
+
+positionAt :: Int -> Int -> Int
+positionAt t depth = head $ drop (t `rem` (length gateCycle)) $ gateCycle
+  where gateCycle = [0..depth-1] ++ (reverse [1..depth-2])
+
+solve = do
+  input <- readFile "input.txt"
+  let gates = readInput input
+  putStrLn "Part 1:"
+  putStrLn $ show $ severity gates 0
+  putStrLn "\nPart 2:"
+  putStrLn $ show $ head $ filter ((==0) . severity gates) [10..]
+
+testInput = "0: 3\n1: 2\n4: 4\n6: 4"
