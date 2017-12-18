f7276551e3f27f1c64cf5b83cb0ec369e004cc55
Michiel Koning <m.g.koning@gmail.com>
2017-12-18 18:30:48 +0100

    day 18: duet, part 2; changed part 1 solution, no longer matches text, but it does work for my input

--- day-18-duet/duet.hs
+++ day-18-duet/duet.hs
@@ -3,25 +3,35 @@ import Data.Map.Strict (Map, empty)
 import Data.Maybe
 import qualified Data.Map.Strict as Map
 
-data State = State { programPointer :: Int, lastSound :: Int, registers :: Map Char Int, recovered :: [Int] } deriving (Show)
+data State = State {
+    programPointer :: Int,
+    outbox :: [Int],
+    registers :: Map Char Int,
+    blocked :: Bool,
+    inbox :: [Int],
+    sent :: Int
+  } deriving (Show)
+
+emptyState = State 0 [] Map.empty False [] 0
+
 data Operand = Register Char | Value Int deriving (Show)
-data Operation = Sound Operand |
+data Operation = Send Operand |
                  Set Operand Operand | 
                  Add Operand Operand | 
                  Multiply Operand Operand |
                  Modulo Operand Operand |
-                 Recover Operand |
+                 Receive Operand |
                  JumpGreaterZero Operand Operand
                  deriving (Show)
 
 readInstruction s =
   case op of
-    "snd" -> Sound firstOperand
+    "snd" -> Send firstOperand
     "set" -> Set firstOperand secondOperand
     "add" -> Add firstOperand secondOperand
     "mul" -> Multiply firstOperand secondOperand
     "mod" -> Modulo firstOperand secondOperand
-    "rcv" -> Recover firstOperand
+    "rcv" -> Receive firstOperand
     "jgz" -> JumpGreaterZero firstOperand secondOperand
     where (op:rest) = words s
           firstOperand = readOperand $ head rest
@@ -35,22 +45,35 @@ readOperand s
 
 execute i state =
   case i of
-    Sound x -> next `seq` state { programPointer = next, lastSound = getValue x registry }
+    Send x -> let val = getValue x registry
+                  newSent = 1 + (sent state)
+              in val `seq` next `seq` newSent `seq`
+                 state { programPointer = next, outbox = val:(outbox state), sent = newSent }
     Set (Register x) y ->
-      next `seq` state { programPointer = next, registers = Map.alter (\_ -> Just $ getValue y registry) x registry }
+              let valY = getValue y registry
+                  newRegistry = setValue x valY registry
+              in next `seq` state { programPointer = next, registers = newRegistry }
     Add (Register x) y -> performOp (+) x y registry
     Multiply (Register x) y -> performOp (*) x y registry
     Modulo (Register x) y -> performOp mod x y registry
     JumpGreaterZero x y ->
-      let jump = (programPointer state) + if getValue x registry > 0 then getValue y registry else 1
+      let pointer = (programPointer state)
+          jump = pointer + if getValue x registry > 0 then getValue y registry else 1
       in jump `seq` state { programPointer = jump }
-    Recover x -> if getValue x registry /= 0 then state { recovered = (lastSound state):(recovered state) } else state
+    Receive (Register x) ->
+      if null $ inbox state
+        then state { blocked = True }
+        else
+          let m:ms = inbox state
+          in state { programPointer = next, inbox = ms, registers = setValue x m registry }
   where
     next = (programPointer state) + 1
     registry = registers state
     performOp op x y registry = 
-      let newValue atX = let result = fromMaybe 0 atX `op` getValue y registry in result `seq` Just result
+      let newValue atX = let result = fromMaybe 0 atX `op` getValue y registry
+                         in result `seq` Just result
       in next `seq` state { programPointer = next, registers = Map.alter newValue x registry }
+    setValue x y registry = Map.alter (\_ -> Just $ y) x registry
 
 getValue x state =
   case x of
@@ -58,11 +81,31 @@ getValue x state =
     Register r -> Map.findWithDefault 0 r state
 
 run instructions state 
-  | not $ null $ getRecovered = head getRecovered
+  | blocked state = state
   | otherwise = run instructions $ execute (instructions !! (programPointer state)) state
-  where getRecovered = take 1 (recovered state) 
+
+stopped instructions state =
+  let next = programPointer state
+      stuck = blocked state && (null $ outbox state)
+  in next < 0 || length instructions <= next || stuck
+
+runPart2 instructions = runPart2' (thread0, thread1)
+  where
+    thread0 = emptyState { registers = Map.singleton 'p' 0 }
+    thread1 = emptyState { registers = Map.singleton 'p' 1 }
+    runPart2' (t0, t1)
+      | all (stopped instructions) [t0, t1] = (t0, t1)
+      | otherwise = runPart2' (newT0, newT1)
+        where
+          newT0 = run instructions $
+            t0 { blocked = False, inbox = reverse $ outbox t1, outbox = [] }
+          newT1 = run instructions $
+            t1 { blocked = False, inbox = reverse $ outbox t0, outbox = [] }
 
 solve = do
   putStrLn "Part 1:"
   instructions <- (map readInstruction . lines) <$> readFile "input.txt"
-  putStrLn $ show $ run instructions (State 0 0 Map.empty [])
+  putStrLn $ show $ head $ outbox $ run instructions emptyState
+  putStrLn "\nPart 2:"
+  let (thread0, thread1) = runPart2 instructions
+  putStrLn $ show $ sent thread1
